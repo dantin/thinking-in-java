9e091ea265aa62a12032a6db2ee6b7cbe1b59742
David Ding <chengjie.ding@gmail.com>
2014-01-16 19:22:13 +0800

    static import

---
+++ src/operators/HelloDate.java
@@ -0,0 +1,12 @@
+//: operators/HelloDate.java
+import static net.mindview.util.Print.print;
+
+import java.util.Date;
+
+public class HelloDate {
+
+    public static void main(String[] args) {
+	print("Hello, it's: ");
+	print(new Date());
+    }
+}
