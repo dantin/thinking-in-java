8076b6d705d4e9d11e5b7a14fd3cc0093e1b6cfe
David Ding <chengjie.ding@gmail.com>
2014-01-17 07:42:19 +0800

    将===和!=使用在对象中

---
+++ src/operators/Equivalence.java
@@ -0,0 +1,13 @@
+//: options/Equivalence.java
+
+public class Equivalence {
+
+    public static void main(String[] args) {
+	Integer n1 = new Integer(47);
+	Integer n2 = new Integer(47);
+	System.out.println("new Integer n1 = " + n1 + ", new Integer n2 = " + n2);
+	System.out.println("n1 == n2: " + (n1 == n2));
+	System.out.println("n1 != n2: " + (n1 != n2));
+    }
+
+}
