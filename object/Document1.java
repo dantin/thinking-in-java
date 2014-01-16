f875f48ca91581ef1ea05fd89060de77d90f5e8f
David Ding <chengjie.ding@gmail.com>
2014-01-16 14:39:17 +0800

    show java document

---
+++ object/Document1.java
@@ -0,0 +1,17 @@
+//: object/Document1.java
+
+/**
+ * A class comment
+ */
+public class Document1 {
+    /**
+     * A field comment
+     */
+    public int i;
+
+    /**
+     * A method comment
+     */
+    public void f() {
+    }
+}
---
+++ object/Document2.java
@@ -0,0 +1,10 @@
+//: object/Document2.java
+
+/**
+ * <pre>
+ * System.out.println(new Date());
+ * </pre>
+ */
+public class Document2 {
+
+}
