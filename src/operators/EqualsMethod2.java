38920bc93056ebf8ca380c42edc5485e80730da0
David Ding <chengjie.ding@gmail.com>
2014-01-17 07:52:58 +0800

    自定义类型使用equals()方法

---
+++ src/operators/EqualsMethod2.java
@@ -0,0 +1,17 @@
+//: operators/EqualsMethod2
+// Default equals() does not compare contents.
+
+class Value {
+    int i;
+}
+
+public class EqualsMethod2 {
+
+    public static void main(String[] args) {
+	Value v1 = new Value();
+	Value v2 = new Value();
+	v1.i = v2.i = 100;
+	System.out.println("Default equals(), v1.equals(v2): " + v1.equals(v2));
+    }
+
+}
