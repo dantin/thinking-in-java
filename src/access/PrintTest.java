04b5680e1dfe74290da06643c8a6f0ce5c20d60a
David Ding <chengjie.ding@gmail.com>
2014-01-20 07:41:34 +0800

    使用定制类

---
+++ src/access/PrintTest.java
@@ -0,0 +1,14 @@
+//: access/PrintTest.java
+// Uses the static printing methods in Print.java.
+import static net.mindview.util.Print.print;
+
+public class PrintTest {
+
+    public static void main(String[] args) {
+	print("Available from now on!");
+	print(100);
+	print(100L);
+	print(3.14159);
+    }
+
+}
