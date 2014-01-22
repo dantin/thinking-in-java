33431f29756fa782bc947f9a0b2aad18fa6b92e0
David Ding <chengjie.ding@gmail.com>
2014-01-23 07:31:09 +0800

    向上转型

---
+++ src/polymorphism/music/Instrument.java
@@ -0,0 +1,12 @@
+//: polymorphism/music/Instrument.java
+package music;
+
+import static net.mindview.util.Print.print;
+
+public class Instrument {
+
+    public void play(Note n) {
+	print("Instrument.play()");
+    }
+
+}
