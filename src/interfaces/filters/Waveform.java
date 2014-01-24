23dfa69ef63013176d0f59198d290aee7da0e7fe
David Ding <chengjie.ding@gmail.com>
2014-01-25 00:55:29 +0800

    相似接口的类库

---
+++ src/interfaces/filters/Waveform.java
@@ -0,0 +1,13 @@
+//: interfaces/filters/Waveform.java
+package filters;
+
+public class Waveform {
+
+    private static long counter;
+    private final long id = counter++;
+
+    public String toString() {
+	return "Waveform " + id;
+    }
+
+}
