6aca44b27d0fcb17c31986e5b7e3bae1b9a63e4e
David Ding <chengjie.ding@gmail.com>
2014-01-25 12:12:28 +0800

    接口中的域可以用非常量表达式初始化

---
+++ src/interfaces/TestRandVals.java
@@ -0,0 +1,14 @@
+//: interfaces/TestRandVals.java
+
+import static net.mindview.util.Print.print;
+
+public class TestRandVals {
+
+    public static void main(String[] args) {
+	print(RandVals.RANDOM_INT);
+	print(RandVals.RANDOM_LONG);
+	print(RandVals.RANDOM_FLOAT);
+	print(RandVals.RANDOM_DOUBLE);
+    }
+
+}
