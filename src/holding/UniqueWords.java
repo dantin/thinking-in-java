491ff4aa64658cfeaa541eadfa1bf0defb02a3f1
David Ding <chengjie.ding@gmail.com>
2014-01-31 05:57:49 +0800

    字典序排序的TreeSet

---
+++ src/holding/UniqueWords.java
@@ -0,0 +1,16 @@
+//: holding/UniqueWords.java
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import net.mindview.util.TextFile;
+
+public class UniqueWords {
+
+    public static void main(String[] args) {
+	Set<String> words = new TreeSet<String>(new TextFile(
+							     "SetOperations.java", "\\W+"));
+	System.out.println(words);
+    }
+
+}
