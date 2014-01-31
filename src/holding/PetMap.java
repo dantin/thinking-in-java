64f49d23b577133ff3230b79b0ef31b488c19b72
David Ding <chengjie.ding@gmail.com>
2014-02-01 03:24:25 +0800

    Map中的containsKey()和containsValue()

---
+++ src/holding/PetMap.java
@@ -0,0 +1,27 @@
+//: holding/PetMap.java
+
+import static net.mindview.util.Print.print;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import pets.Cat;
+import pets.Dog;
+import pets.Hamster;
+import pets.Pet;
+
+public class PetMap {
+
+    public static void main(String[] args) {
+	Map<String, Pet> petMap = new HashMap<String, Pet>();
+	petMap.put("My Cat", new Cat("Molly"));
+	petMap.put("My Dog", new Dog("Ginger"));
+	petMap.put("My Hamster", new Hamster("Bosco"));
+	print(petMap);
+	Pet dog = petMap.get("My Dog");
+	print(dog);
+	print(petMap.containsKey("My Dog"));
+	print(petMap.containsValue(dog));
+    }
+
+}
