d2b8b4d5077e0b30d9542a4f8311354a1142f3e1
David Ding <chengjie.ding@gmail.com>
2014-02-01 05:07:50 +0800

    各种不同容器类的方法差异

---
+++ src/holding/ContainerMethods.java
@@ -0,0 +1,11 @@
+//: holding/ContainerMethods.java
+
+import net.mindview.util.ContainerMethodDifferences;
+
+public class ContainerMethods {
+
+    public static void main(String[] args) {
+	ContainerMethodDifferences.main(args);
+    }
+
+}
