ece14730f33d61db46d7a0309da22cc234b558df
David Ding <chengjie.ding@gmail.com>
2014-02-02 06:33:10 +0800

    没有finally块，资源不保证释放

---
+++ src/exceptions/OnOffException1.java
@@ -0,0 +1,5 @@
+//: exceptions/OnOffException1.java
+
+public class OnOffException1 extends Exception {
+
+}
---
+++ src/exceptions/OnOffException2.java
@@ -0,0 +1,5 @@
+//: exceptions/OnOffException2.java
+
+public class OnOffException2 extends Exception {
+
+}
