9474e9edc675226cb5d25c7cf39a495a10e8471d
David Ding <chengjie.ding@gmail.com>
2014-02-07 08:13:57 +0800

    Pattern和Matcher

---
+++ src/strings/TestRegularExpression.java
@@ -0,0 +1,30 @@
+//: strings/TestRegularExpression.java
+// Allows you to easily try out regular expressions.
+// {Args: abcabcabcdefabc "abc+" "(abc)+" "(abc){2,}" }
+
+import static net.mindview.util.Print.print;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestRegularExpression {
+
+    public static void main(String[] args) {
+	if (args.length < 2) {
+	    print("Usage:\njava TestRegularExpression "
+		  + "characterSequence regularExpression+");
+	    System.exit(0);
+	}
+	print("Input: \"" + args[0] + "\"");
+	for (String arg : args) {
+	    print("Regular expression: \"" + arg + "\"");
+	    Pattern p = Pattern.compile(arg);
+	    Matcher m = p.matcher(args[0]);
+	    while (m.find()) {
+		print("Match \"" + m.group() + "\" at positions " + m.start()
+		      + "-" + (m.end() - 1));
+	    }
+	}
+    }
+
+}
